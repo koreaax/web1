4be4e3c360551b3dab4448e8085d156bfa629cdc
koreaax <koreaax3@naver.com>
2021-12-23 11:18:30 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url')
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    var pathName = url.parse(_url,true).pathname;
+
+    if(pathName === '/'){
+        if(queryData.id === undefined){
+            var title = 'WELCOME'
+            var descirption ='hello node.js'
+            var template = `
+            <!doctype html>
+            <html>
+                <head>
+                <title>WEB1 - ${title} </title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p> ${descirption}
+                </p>
+                </body>
+            </html>
+            `
+            response.writeHead(200);
+            response.end(template);
+        }else {
+            fs.readFile(`data/${queryData.id}`,'utf-8', (err , descirption) => {
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                    <head>
+                    <title>WEB1 - ${title} </title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p> ${descirption}
+                    </p>
+                    </body>
+                </html>
+                `
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    }else{
+        response.writeHead(404);
+        response.end('Not Found');
+    }
+});
+app.listen(3000);
